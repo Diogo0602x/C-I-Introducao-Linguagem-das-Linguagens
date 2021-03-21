42346ceca9b408bbe70a20d1302558c775d4cf56
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-20 22:40:19 -0300

    numeroAteCem criado

---
+++ numeroAteCem.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  // repare no menor-igual
+  for(int i = 1; i <= 100; i++) {
+    printf("%d \n", i);
+  }
+  //while(numero <= 100) {
+  //  printf("%d\n ", numero);
+  //  numero++;
+  //}
+}
